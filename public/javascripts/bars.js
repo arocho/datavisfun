4da84d3e9d0c04ba69d198f65b0ad2015587ce17
Allison G. Martinez-Arocho <martinga@us.ibm.com>
2017-07-20 00:03:49 -0600

    Displaying bar graph

--- public/javascripts/bars.js
+++ public/javascripts/bars.js
@@ -1,9 +1,95 @@
-var data = [30, 86, 168, 281, 303, 365];
+/*BAR CHART*/
+var margin = {top: 80, right: 180, bottom: 80, left: 180},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-d3.select(".chart")
-  .selectAll("div")
-  .data(data)
+var x = d3.scaleBand()
+    .rangeRound([0, width], .1, .3);
+
+var y = d3.scaleLinear()
+    .range([0, height]);
+
+var xAxis = d3.axisBottom(x);
+
+var yAxis = d3.axisLeft(y)
+    .ticks(5);
+
+var svg = d3.select(".barchart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var mutationTypes = getListOfMutationTypes(dataObj.all_data);
+var countOfTypes = countProperties(mutationTypes);
+console.log("THIS IS THE COUNT: " + countOfTypes);
+
+if(countOfTypes) {
+    var jsonCountObj = JSON.parse(countOfTypes);
+
+    jsonCountObj.forEach(function(d) {
+        d.type = d.type;
+        d.count = +d.count;
+    });
+
+  x.domain(jsonCountObj.map(function(d) { return d.type; }));
+  y.domain([d3.max(jsonCountObj, function(d) { return d.count; }), 0]);
+
+  svg.append("text")
+      .attr("class", "title")
+      .attr("x", 50)
+      .attr("y", -26)
+      .text("Count of Chromosome Mutations per Type");
+
+  svg.append("g")
+      .attr("class", "xaxis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll(".tick text")
+      .call(wrap, x.bandwidth());
+
+  svg.append("g")
+      .attr("class", "yaxis")
+      .call(yAxis);
+
+  var bar = svg.selectAll("bar")
+      .data(jsonCountObj)
     .enter()
-    .append("div")
-    .style("width", function(d) { return d + "px"; })
-    .text(function(d) { return d; });
+
+      bar.append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.type) + 20; })
+      .attr("width", x.bandwidth()-20)
+      .attr("y", function(d) { return y(parseInt(d.count)); })
+      .attr("height", function(d) { return height - y(parseInt(d.count)); });
+
+  bar.append("text")
+      .attr("x", function(d) { return (x(d.type))+ x.bandwidth()/2;})
+      .attr("y", function(d) { return y(d.count) + 3; })
+      .attr("dy", ".75em")
+      .text(function(d) { return d.count; });
+}
+
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
--- public/javascripts/process-data.js
+++ public/javascripts/process-data.js
@@ -1,29 +1,3 @@
-//local constants
-var GBMUS_SERVER = 'https://dcc.icgc.org';
-var GBMUS_PATH = '/api/v1/projects/GBM-US/mutations';
-var GBMUS_PARAMS = '?field=id,mutation,type,chromosome,start,end&size=100&order=desc'
-
-var dataObj = new Object();
-
-/*
-*  Calls the mutations API endpoint of the ICGC Data Coordination Center and sends response to be parsed
-*/
-function makeRequest() {
-    d3.json(GBMUS_SERVER + GBMUS_PATH + GBMUS_PARAMS, function(error, data) {
-        if(error) {
-            var error = '{ "status":"Internal Server Error", "statusCode":"500", ' +
-                '"error":"Error retrieving subscriptions API response." }';
-            var jsonResponse = JSON.parse(error);
-            console.log(jsonResponse);
-        }
-        else {
-            dataObj.all_data = data.hits;
-            console.log(dataObj.all_data);
-            return dataObj.all_data;
-        }
-    });
-}
-
 function getListOfMutationTypes(dataArr) {
     var typeCountArr = [];
     for (var i = 0; i < dataArr.length; i++) {
@@ -37,15 +11,24 @@ function getListOfMutationTypes(dataArr) {
 function countProperties(propertiesArr) {
     var counts = {};
     for (var i = 0; i < propertiesArr.length; i++) {
-        counts[propertiesArr[i]] = 1 + (counts[propertiesArr[i]] || 0);
+        var name = propertiesArr[i];
+        if(counts[name]) {
+            counts[name] = counts[name] + 1
+        }
+        else {
+            counts[name] = 1;
+        }
     }
-    console.log(propertiesArr.length);
-    console.log(counts);
-    var countOfProperties = d3.values(counts);
-    console.log(countOfProperties);
-    return countOfProperties;
-}
 
-function getAllData() {
-    return dataObj.all_data;
+    var countsKeys = Object.keys(counts);
+    var dataArr = [];
+    for (var i = 0; i < countsKeys.length; i++) {
+        dataArr.push({
+            "type": countsKeys[i],
+            "count": Object.values(counts)[i]
+        });
+    }
+    var jsonDataArr = JSON.stringify(dataArr);
+    console.log(jsonDataArr);
+    return jsonDataArr;
 }
